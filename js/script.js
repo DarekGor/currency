019c93bee77e32cf487f5dfd587d39fcfbc24b47
Darek Gorecki <goreckidariusz25@gmail.com>
2021-05-12 19:47:17 +0200

    Initail commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+let formElement = document.querySelector(".js-form");
+let amountElement = document.querySelector(".js-amount");
+let currencyElement = document.querySelector(".js-currency");
+let resultElement = document.querySelector(".js-result");
+
+let rateEUR = 4.57;
+let rateUSD = 3.81;
+let rateGBP = 5.30;
+
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let amount = +amountElement.value;
+    let currency = currencyElement.value;
+
+    let result;
+
+    switch (currency) {
+        case "EUR":
+            result = amount / rateEUR;
+            break;
+
+        case "USD":
+            result = amount / rateUSD;
+            break;
+
+        case "GBP":
+            result = amount / rateGBP;
+            break;
+    }
+
+    resultElement.innerText = `Po przeliczeniu otrzymasz: ${result.toFixed(
+        2
+    )} ${currency}`;
+});
